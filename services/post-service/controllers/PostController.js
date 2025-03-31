274e9c1667b66988a280e5696be24ca1bb84b4fd
HuuphucPro <tranhuuphuc599@gmail.com>
2025-04-01 02:47:23 +0700

    api get usernameandpostid post-service

--- services/post-service/controllers/PostController.js
+++ services/post-service/controllers/PostController.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const { validationResult } = require('express-validator');
 const UserModel = require('../models/UserModel');
 
+
+
 const createPost = async (req, res) => {
 
      try {
@@ -12,10 +14,8 @@ const createPost = async (req, res) => {
 
           // Lấy userId từ token đã đăng nhập
           const luserID = req.user.userID;
-
-
-          console.log(luserID)
-
+          const username = req.user.username;
+          console.log("id người đăng bài: ", luserID)
           const existingUser = await UserModel.findOne({ luserID });
 
           if (existingUser) {
@@ -25,6 +25,7 @@ const createPost = async (req, res) => {
           // Tạo bài viết mới
           const newPost = new PostModel({
                userID: luserID,
+               username: username,
                content,
                imageUrl
 
@@ -40,4 +41,35 @@ const createPost = async (req, res) => {
      }
 };
 
-module.exports = { createPost };
+// Lấy bài đăng theo ID
+const getPostByUsernameAndPostId = async (req, res) => {
+     try {
+
+          const { username, postId } = req.params;
+
+          console.log("username đăng bài: ", username)
+          console.log("id bài đăng: ", postId)
+
+          if (!mongoose.Types.ObjectId.isValid(postId)) {
+               return res.status(400).json({ message: 'ID bài đăng không hợp lệ' });
+          }
+
+          const user = await UserModel.findOne({ username: username });
+          if (!user) {
+               return res.status(404).json({ message: 'Người dùng không tồn tại' });
+          }
+
+          const post = await PostModel.findOne({_id: postId });
+          if (!post) {
+               return res.status(404).json({ message: 'Bài đăng không tồn tại' });
+          }
+
+          return res.status(200).json({ post });
+     } catch (error) {
+          console.error('Lỗi lấy bài đăng:', error);
+          return res.status(500).json({ message: 'Lỗi server lấy bài đăng' });
+     }
+};
+
+
+module.exports = { createPost, getPostByUsernameAndPostId};
--- services/post-service/routes/PostRoute.js
+++ services/post-service/routes/PostRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const postMiddleware = require('../middleware/PostMiddleware');
-const { createPost } = require('../controllers/PostController');
+const { createPost, getPostByUsernameAndPostId } = require('../controllers/PostController');
 const { route } = require('../../auth-service/routes/AuthRoute');
-
 const router = express.Router();
 
+
 router.post("/create", postMiddleware, createPost);
+router.get("/:username/:postId", getPostByUsernameAndPostId);
 
 
 module.exports = router
